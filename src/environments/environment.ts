00369cec43b7989b7d73d99da21be2076239a53d
saadAletek <said.elatik.602@gmail.com>
2025-03-24 22:20:21 +0300

    aa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,12 @@
 export const environment = {
     production: false,
     firebaseConfig : {
-        apiKey: "AIzaSyDz-Zx08E5AM2DH-sZAEoMAChK-EXBLS0E",
-        authDomain: "portifolio-2a221.firebaseapp.com",
-        databaseURL: "https://portifolio-2a221-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "portifolio-2a221",
-        storageBucket: "portifolio-2a221.firebasestorage.app",
-        messagingSenderId: "366488533834",
-        appId: "1:366488533834:web:10b883bac3f4082037dfb3",
-        measurementId: "G-F8EL95PSBP"
+        apiKey: "AIzaSyCI1vEOsx476scp_MRx9VRwHGW0C_b41F4",
+        authDomain: "blogsprototype.firebaseapp.com",
+        projectId: "blogsprototype",
+        storageBucket: "blogsprototype.firebasestorage.app",
+        messagingSenderId: "505684034394",
+        appId: "1:505684034394:web:e7f8487ac21d4f72556963",
+        measurementId: "G-WB82RXLRTT"
       }
   };
